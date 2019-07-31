e37bd3ac5ef2234c77df512e94fcdff4c2cb8da4
Simão Soares <s.soares@moliporex.pt>
2019-07-31 10:23:17 +0100

    added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
+const del = require('del');
 const connect = require('gulp-connect');
 const CacheBuster = require('gulp-cachebust');
 
 const cachebust = new CacheBuster();
+
+function clean() {
+    return del('dist');
+}
 
 function css() {
     return gulp.src('styles/*.css')
@@ -24,6 +29,7 @@ function server(done) {
     });
 }
 
+exports.clean = clean;
 exports.css = css;
 exports.html = html;
 exports.server = server;
@@ -31,4 +37,4 @@ exports.server = server;
 /*
  * Define default task that can be called by just running `gulp` from cli
  */
-exports.default = gulp.series(css, html, server);
+exports.default = gulp.series(clean, css, html, server);
